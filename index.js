9c480d8bf0192c60ba3f09283645c0810cace730
marzouk-marzouk <99255709+mostafamarzouk88@users.noreply.github.com>
2025-04-26 18:54:10 +0300

    clock

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () { 
+
+    function getCurrentDate() {
+        // Get the current date
+        let date = new Date();
+        // get hour, minute, second
+        let hour = date.getHours();
+        let min = date.getMinutes();
+        let sec = date.getSeconds(); 
+        // AM or PM settings
+        
+     }
+
+})

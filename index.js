c918892f4b4b829cf7872862d26d1f8cd48ff78d
marzouk-marzouk <99255709+mostafamarzouk88@users.noreply.github.com>
2025-04-26 19:45:59 +0300

    2

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
-$(document).ready(function () { 
+$(document).ready(function () {
 
-    function getCurrentDate() {
-        // Get the current date
-        let date = new Date();
-        // get hour, minute, second
-        let hour = date.getHours();
-        let min = date.getMinutes();
-        let sec = date.getSeconds(); 
-        // AM or PM settings
-        
-     }
 
-})
+
+  function getCurrentDate() {
+    // Get the current date
+    let date = new Date();
+    // get hour, minute, second
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    // AM or PM settings
+    let ampm = "AM";
+
+    if (hour > 12) {
+      hour = hour - 12;
+      ampm = "PM";
+    }
+    if (hour == 0) {
+      hour = 12;
+      }
+      
+      // Set the timeout to call the function again after 1 second
+      
+      
+      
+      displayDate(hour, min, sec, ampm);
+      setTimeout(getCurrentDate, 1000);
+    }
+    
+    
+    let displayDate = function (hour, min, sec, ampm) {
+        $("#hours").text(hour);
+        $("#min").text(min);
+        $("#sec").text(sec);
+        $(document).on("click", function () {
+            console.log("clicked");
+        });
+    };
+    getCurrentDate();
+    
+});
